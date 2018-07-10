53dac7958abe358bcf88b927d1a5cc76674d65ea
yanggongzhou <350926623@qq.com>
2018-07-11 07:43:48 +0800

    登录页框架样式1

--- js/jd.js
+++ js/jd.js
@@ -137,32 +137,27 @@ $(function () {
     // })
 
     // 京东秒杀倒计时
-    var sj = new Date();
-    var sjHour=23-sj.getHours();
-    var sjMin = 59-sj.getMinutes();
-    var sjSecond=59-sj.getSeconds();
+     setInterval(function () {
+         var sj = new Date();
+         var sjHour=23-sj.getHours();
+         var sjMin = 59-sj.getMinutes();
+         var sjSecond=59-sj.getSeconds();
 
-    window.onload =function(){
-        setInterval(
-            function(){
-                if(sjHour>=10){
-                    $("#jdms-hour").html(sjHour);
-                }else{
-                    $("#jdms-hour").html("0"+sjHour);
-                };
-                if(sjMin>=10){
-                    $("#jdms-min").html(sjMin);
-                }else{
-                    $("#jdms-min").html("0"+sjMin);
-                };
-                if(sjSecond>=10){
-                    $("#jdms-second").html(sjSecond);
-                }else{
-                    $("#jdms-second").html("0"+sjSecond);
-                };
-            }
-         ,1000);
-    }
-
+         if(sjHour>=10){
+             $("#jdms-hour").html(sjHour);
+         }else{
+             $("#jdms-hour").html("0"+sjHour);
+         }
+         if(sjMin>=10){
+             $("#jdms-min").html(sjMin);
+         }else{
+             $("#jdms-min").html("0"+sjMin);
+         }
+         if(sjSecond>=10){
+             $("#jdms-second").html(sjSecond);
+         }else{
+             $("#jdms-second").html("0"+sjSecond);
+         }
+     },1000);
 
 });
